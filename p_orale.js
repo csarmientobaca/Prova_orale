014dcb3506220775b95bbeb1f7f0e4dabcdafcc4
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-22 14:38:15 +0100

    prova

---
+++ p_orale.js
@@ -0,0 +1,148 @@
+// // ***********parte 1 de la orale *********
+// const primoObj = {
+//     colore: "blue",
+//     misura: "grande",
+//     codice: 1112233
+// }
+// console.log(primoObj)
+
+// const arraYx = [
+//     1, 2, 3, 5
+// ]
+// const arrNomi = [
+//     "carlos", "julia", "palma"
+// ]
+// console.log(arrNomi[2])
+// console.log(arraYx)
+
+// const arrayDiObj = [
+//     {
+//         codice: "123",
+//         name: "primi tre numeri",
+//         barcode: 445544
+
+//     },
+//     {
+//         codice: "333",
+//         name: "tre volte treee",
+//         barcode: 433334
+
+//     },
+//     {
+//         codice: "999",
+//         name: "six six six alla rovescia",
+//         barcode: 11999
+
+//     },
+
+// ]
+// console.log(arrayDiObj[1])
+
+
+
+// *********funzione creazione*********
+
+// const sommaAeB = (a, b) => {
+//     let AB = 0
+//     if (typeof (a) === "number" && typeof (b) === "number") {
+//         AB = a + b
+//         return AB
+//     }
+//     else {
+//         console.log("its not a valid sum")
+//     }
+// }
+
+// console.log(sommaAeB(13, -12))
+
+
+// const sumOrderNum = (numDaSommare) => {
+//     let sum = 0
+
+//     for (let i = 0; i <= numDaSommare; i++) {
+//         sum += i
+//     }
+//     return sum
+
+// }
+// console.log("risultato fun sumOrderNum:", sumOrderNum(3))
+
+
+
+
+// const student = {
+//     name: "diego",
+//     sur: "noob"
+// }
+// const {
+//     name 
+// } = student
+
+
+// console.log(name)
+
+let i = 0
+while (i < 10) {
+    console.log("hi")
+    i++
+}
+
+hiWorld = () => {
+    console.log("hello wrold".toUpperCase())
+}
+console.log(hiWorld())
+
+
+const arr = [
+    0, 1, 2, 3, 4, 5
+]
+const myname = "hi my name is"
+
+arr.slice(1)
+
+console.log(arr.slice(0, 3))
+
+console.log(myname.substring(3, 5))
+
+
+console.log(myname.substr(3, 5))
+
+console.log(myname.substr(3, 5).toUpperCase())
+
+
+
+// arr.push(6)
+// console.log(arr)
+
+// arr.pop()
+// console.log(arr)
+
+
+// arr.pop()
+// console.log(arr)
+
+
+// arr.pop()
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)
+
+// arr.unshift("zero")
+// console.log(arr)
+
+// arr.splice(0, 1, 0)
+// console.log(arr)
+
+
+const array123 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+]
+
+array123.forEach(element => {
+    element + 1
+    console.log
+
+
+});
+
